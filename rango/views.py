34c628f88deb0063516ad3664f53cf87c8c74005
Rachael Ferguson <2252146F@student.gla.ac.uk>
2022-02-18 14:08:43 +0000

    chapter 4

--- rango/views.py
+++ rango/views.py
@@ -6,3 +6,7 @@ def index(request):
     return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
