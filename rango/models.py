60ab53a2f74492ecedcb5355f10fff032c3f7ded
Rachael Ferguson <2252146F@student.gla.ac.uk>
2022-02-24 12:32:40 +0000

    Completed exercises for chapter 9

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -25,11 +26,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Page(models.Model):
     TITLE_MAX_LENGTH = 128
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=TITLE_MAX_LENGTH)
     url = models.URLField()
     views = models.IntegerField(default=0)
+
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+
+def __str__(self):
+    return self.user.username
